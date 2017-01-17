fda2ac5206635a11fb6b99ff3033bd28a3ce721c
Valentin <valentin_deyringer@web.de>
2017-01-17 12:20:15 +0100

    read sentences more clear

--- src/main/scala/wordcloud/utils/corpus/CorpusReader.scala
+++ src/main/scala/wordcloud/utils/corpus/CorpusReader.scala
@@ -14,9 +14,7 @@ abstract class CorpusReader(corpusFile: String) {
   }
 
   def readSentences(): Iterator[List[Token]] = {
-    val lines = Source.fromFile(corpusFile).getLines
-    val tokenLines = lines.map(x => lineToToken(x))
-    iterSplitAt(tokenLines, (x: Token) => x == Token("", "", "EOS", ""))
+    iterSplitAt(readTokens(), (x: Token) => x == Token("", "", "EOS", ""))
   }
 
 }
