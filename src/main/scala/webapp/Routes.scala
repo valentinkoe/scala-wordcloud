0caacb0de83690e2edc2908e607d0f30e1b18eff
Valentin <valentin_deyringer@web.de>
2017-02-18 16:33:51 +0100

    streaming test

--- src/main/scala/webapp/Routes.scala
+++ src/main/scala/webapp/Routes.scala
@@ -1,15 +1,23 @@
 package webapp
 
 import org.http4s._
+import org.http4s.MediaType._
+import org.http4s.headers.`Content-Type`
 import org.http4s.dsl._
 import org.http4s.server.staticcontent
 import org.http4s.server.staticcontent.ResourceService.Config
 import scalaz.concurrent.Task
 import scalaz.stream.Process
 import scalaz.stream.io.toInputStream
+
 import wordcloud.Processing
 
 import scala.io.Source
+
+import scala.concurrent.duration._
+import scalaz.stream.time
+import scalaz.concurrent.Task
+import scalaz.concurrent.Strategy.DefaultTimeoutScheduler
 
 object Routes {
 
@@ -27,11 +35,15 @@ object Routes {
 
   val service: HttpService = HttpService {
 
+    case GET -> Root / "streaming" =>
+      // Its also easy to stream responses to clients
+      Ok(dataStream(100))
+
     case r @ POST -> Root / "tokens" :? LangQueryParamMatcher(lang)
                                        +& AdjQueryParamMatcher(adj)
                                        +& NounQueryParamMatcher(noun) =>
       val inp = Source.fromInputStream(toInputStream(r.body), "UTF-8")
-      Ok(tokenStream(inp, lang, adj, noun))
+      Ok(tokenStream(inp, lang, adj, noun)).withContentType(Some(`Content-Type`(`text/plain`)))
 
     // retrieves static resources
     case r @ GET -> _ if r.pathInfo.startsWith("/static") => static(r)
@@ -56,4 +68,11 @@ object Routes {
   def tokenStream(text: Iterator[Char], lang: String, adj: Boolean=false, noun: Boolean=true): Process[Task, String] =
     toStream(Processing.getTokens(text, lang, adj, noun))
 
+  def dataStream(n: Int): Process[Task, String] = {
+    implicit def defaultScheduler = DefaultTimeoutScheduler
+    val interval = 1.second
+    time.awakeEvery(interval)
+      .map(_ => s"Current system time: ${System.currentTimeMillis()} ms\n")
+      .take(n)
+  }
 }
